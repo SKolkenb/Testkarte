ad2befa2886dc093bc4ded3414a04c43e328de71
SKolkenb <143170876+SKolkenb@users.noreply.github.com>
2023-08-25 10:23:03 +0200

    erster Upload

---
+++ data/RettungswachenAachen_2.js
@@ -0,0 +1,15 @@
+var json_RettungswachenAachen_2 = {
+"type": "FeatureCollection",
+"name": "RettungswachenAachen_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "7", "Abk": "RW7", "Name": "Rettungswache West (MHD)" }, "geometry": { "type": "Point", "coordinates": [ 6.0566719, 50.769140799999981 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Abk": "FuRW3", "Name": "Feuer- und Rettungswache 3 (BF)" }, "geometry": { "type": "Point", "coordinates": [ 6.0485008, 50.7889132 ] } },
+{ "type": "Feature", "properties": { "id": "1", "Abk": "FuRW1", "Name": "Feuer- und Rettungswache 1 (BF)" }, "geometry": { "type": "Point", "coordinates": [ 6.1160084, 50.777011599999987 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Abk": "RW5", "Name": "Rettungswache 5 (JUH)" }, "geometry": { "type": "Point", "coordinates": [ 6.1294676, 50.780708 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Abk": "RW6", "Name": "Rettungswache 6 (DRK)" }, "geometry": { "type": "Point", "coordinates": [ 6.097691, 50.7803393 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Abk": "FuRW 2", "Name": "Feuer- und Rettungswache 2 (BF)" }, "geometry": { "type": "Point", "coordinates": [ 6.1755199, 50.7294762 ] } },
+{ "type": "Feature", "properties": { "id": "10", "Abk": "RW10", "Name": "Rettungswache 10 (DRK)" }, "geometry": { "type": "Point", "coordinates": [ 6.064613386446086, 50.791574668578569 ] } },
+{ "type": "Feature", "properties": { "id": "9", "Abk": "RW9", "Name": "Rettungswache 9 (JUH)" }, "geometry": { "type": "Point", "coordinates": [ 6.134386570291535, 50.714991767016869 ] } }
+]
+}
